a812ff5cfbcf734853665777bbf256c0cd8cd3eb
James Green <jgreen.gus@gmail.com>
2024-11-15 15:54:34 -0500

    Updated ContactForm.js with input fields

--- src/pages/ContactForm.js
+++ src/pages/ContactForm.js
@@ -2,6 +2,12 @@ import { Button, Label, Col, FormGroup } from 'reactstrap';
 import { Formik, Field, Form } from 'formik';
 
 const ContactForm = () => {
+    const handleSubmit = (values, { resetForm }) => {
+        console.log('form values:', values);
+        console.log('in JSON format:', JSON.stringify(values));
+        resetForm();
+    }
+
     return (
         <Formik
             initialValues={{
@@ -13,6 +19,7 @@ const ContactForm = () => {
                 contactType: 'By Phone',
                 feedback: ''
             }}
+            onSubmit={handleSubmit}
         >
 
             <Form>
@@ -21,7 +28,9 @@ const ContactForm = () => {
                         First Name
                     </Label>
                     <Col md='10'>
-                        <Field 
+                        <Field
+                            name='firstName'
+                            placeholder="First Name"
                             className='form-control'
                         />
                     </Col>
@@ -31,7 +40,11 @@ const ContactForm = () => {
                         Last Name
                     </Label>
                     <Col md='10'>
-
+                        <Field
+                            name='lastName'
+                            placeholder="Last Name"
+                            className='form-control'
+                        />
                     </Col>
                 </FormGroup>
                 <FormGroup row>
@@ -39,7 +52,11 @@ const ContactForm = () => {
                         Phone
                     </Label>
                     <Col md='10'>
-
+                        <Field
+                            name='phone'
+                            placeholder="Phone"
+                            className='form-control'
+                        />
                     </Col>
                 </FormGroup>
                 <FormGroup row>
@@ -47,15 +64,33 @@ const ContactForm = () => {
                         Email
                     </Label>
                     <Col md='10'>
-
+                        <Field
+                            name='email'
+                            placeholder="Email"
+                            type="email"
+                            className='form-control'
+                        />
                     </Col>
                 </FormGroup>
                 <FormGroup row>
                     <Label check md={{ size: 4, offset: 2 }}>
+                        <Field
+                            name='agree'
+                            type='checkbox'
+                            className='form-check-input'
+                        />{' '}
                         May we contact you?
                     </Label>
                     <Col md='4'>
+                        <Field
+                            name='contactType'
+                            as='select'
+                            className='form-control'
+                        >
+                            <option>By Phone</option>
+                            <option>By Email</option>
 
+                        </Field>
                     </Col>
                 </FormGroup>
                 <FormGroup row>
@@ -63,12 +98,22 @@ const ContactForm = () => {
                         Your Feedback
                     </Label>
                     <Col md='10'>
-
+                        <Field
+                            name='feedback'
+                            as='textarea'
+                            rows='12'
+                            className='form-control'
+                        />
+                    </Col>
+                </FormGroup>
+                <FormGroup>
+                    <Col md={{ size: 10, offset: 2 }}>
+                        <Button type='submit' color='primary'>
+                            Send Feedback
+                        </Button>
                     </Col>
                 </FormGroup>
             </Form>
-
-
         </Formik>
     )
 };
