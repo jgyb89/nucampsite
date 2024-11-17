bfcc660715ddc476ad88703bbd03716b24a7100b
James Green <jgreen.gus@gmail.com>
2024-11-16 20:08:20 -0500

    CommentForm.js and CommentsList.js build

---
+++ src/utils/validateCommentForm.js
@@ -0,0 +1,15 @@
+export const validateCommentForm = (values) => {
+    const errors = {};
+
+    if (values.author.length < 2) {
+        errors.firstName = 'Must be at least 2 characters.';
+    } else if (values.author.length > 15) {
+        errors.firstName = 'Must be 15 characters or less';
+    }
+
+    if (!values.rating) {
+        errors.rating = 'Required';
+    }
+
+    return errors;
+};
